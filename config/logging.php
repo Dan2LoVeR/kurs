70083516bbfff8d3cf70a6a126a097d8260a6cd4
Dan2LoVeR <valovdaniil2003@gmail.com>
2024-04-12 11:00:07 +0300

    loggging

--- config/logging.php
+++ config/logging.php
@@ -126,6 +126,13 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'deprecations' => [
+            'driver' => 'single',
+            'path' => storage_path('logs/php-deprecation-warnings.log'),
+        ],
     ],
+
+    
 
 ];
