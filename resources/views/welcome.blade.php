b0b3686712bec37a03f72bef6d6d37101ab48375
Dan2LoVeR <valovdaniil2003@gmail.com>
2024-05-27 08:15:35 +0300

    tets

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -121,7 +121,7 @@
                   <div
                       class="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#44BCFF] via-[#FF44EC] to-[#FF675E] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt">
                   </div>
-                  <a href="{{route("lessons")}}" title="Get quote now"
+                  <a href="{{route("finish")}}" title="Get quote now"
                       class="relative inline-flex items-center justify-center px-8 py-4 text-lg font-bold text-black transition-all duration-200 border-black border-2 font-pj rounded-xl focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-900"
                       role="button"> Сделать логотип
                   </a>
